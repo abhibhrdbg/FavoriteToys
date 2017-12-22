a52bac605329a2593325c890a81c7e96d4b71e57
Abhishek Bhardwaj <abhishek@abhibhr.com>
2017-12-22 22:51:44 +0530

    copleted todo for lesson 2 exercise 2

--- app/src/main/java/com/abhibhr/favoritetoys/MainActivity.java
+++ app/src/main/java/com/abhibhr/favoritetoys/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.abhibhr.favoritetoys.utilities.NetworkUtils;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -28,37 +32,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
-    // TODO (5) Set the orderInCategory to 1
-    // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-    // TODO (7) Set the title to the search string ("Search") from strings.xml
-    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
+    // TODO (2) Create a method called makeGithubSearchQuery
+    // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
 
+    void makeGithubSearchQuery(){
+        String githubQueryString = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQueryString);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+    }
 
-    // TODO (8) Override onCreateOptionsMenu
-    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-    // TODO (10) Return true to display your menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
     }
-
-    // TODO (11) Override onOptionsItemSelected
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int menuItemThatWasSelected = item.getItemId();
         if(menuItemThatWasSelected == R.id.action_search){
+            // TODO (4) Remove the Toast message when the search menu item is clicked
             Context context = MainActivity.this;
-            String message = "Search Selected";
-            Toast.makeText(context,message, Toast.LENGTH_LONG).show();
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
